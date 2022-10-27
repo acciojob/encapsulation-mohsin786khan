016d28c7dac0b5a2867be22db4121abd172e9f4f
mohsin khan <khan.mohsin.00001@gmail.com>
2022-10-27 12:02:48 +0530

    encapsulation working

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String args[]) {
+        RWOnly obj = new RWOnly();
+
+        // obj.name = "Mohsin";
+        // java: name has private access in com.driver.RWOnly
+
+         obj.setName("Mohsin");
+         System.out.println(obj.getName());
+    }
 }
